510565e6fd5db1a13c222d45688e759cd160981c
Hankamotz <andimalo@hotmail.es>
2016-07-07 12:07:34 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ var webappUrl = "https://www.messenger.com/login.php"
 
 // the the pattern that defines which links are considered
 // local and which are opened in an external browser (required)
-var webappUrlPattern = "https?://www.messenger.*/*,https?://facebook.*/*,https?://www.facebook.*/*,https?://m.*/*, https?://mobile.*/*, https?://messenger"
+var webappUrlPattern = "https?://www.messenger.*/*, https?://messenger"
 
 // a user agent override (optional)
 // var webappUA = "uncomment this line and set your User Agent string here between these quotes, if you need to override it"
